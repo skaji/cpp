034bb88fd4528a9424a19d626cfb04bff122a6cf
Shoichi Kaji <skaji@cpan.org>
2018-02-10 21:51:39 +0900

    udpate

--- file.cpp
+++ file.cpp
@@ -3,16 +3,38 @@
 #include <fstream>
 #include <sstream>
 
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 using namespace std;
 
 namespace {
+bool slurp(int fd, string& out) {
+  char buf[4096];
+  ostringstream ss;
+  while (1) {
+    ssize_t len = read(fd, buf, sizeof(buf));
+    if (len < 0) {
+      return false;
+    } else if (len == 0) {
+      out.assign(ss.str());
+      return true;
+    } else {
+      ss.write(buf, len);
+    }
+  }
+}
+
+ino_t getInode(int fd) {
+  struct stat buf;
+  return fstat(fd, &buf) == 0 ? buf.st_ino : 0;
+}
+
 ino_t getInode(const string& path) {
   struct stat buf;
-  if (stat(path.c_str(), &buf) == 0) {
-    return buf.st_ino;
-  } else {
-    return 0;
-  }
+  return stat(path.c_str(), &buf) == 0 ? buf.st_ino : 0;
 }
 };
 
@@ -21,21 +43,21 @@ File::File(const string& path) : path_(path), inode_(0) {}
 File::~File() {}
 
 bool File::read(string& out) {
-  ifstream in(path_);
-  if (!in) {
+  int fd = open(path_.c_str(), O_RDONLY);
+  if (fd < 0) {
     return false;
   }
-  ostringstream ss;
-  ss << in.rdbuf();
-  in.close();
-  out.assign(ss.str());
-  inode_ = getInode(path_);
-  return true;
+  bool ok = slurp(fd, out);
+  if (ok) {
+    inode_ = getInode(fd);
+  }
+  close(fd);
+  return ok;
 }
 
 bool File::isReplaced() const {
   if (inode_ == 0) {
     return true;
   }
-  return getInode(path_) != inode_;
+  return inode_ != getInode(path_);
 }
