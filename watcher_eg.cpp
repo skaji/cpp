f7fafb2b320be88855c3b68e7f2c2eaf9d76fcbb
Shoichi Kaji <skaji@cpan.org>
2018-09-22 23:18:41 +0900

    update

--- watcher_eg.cpp
+++ watcher_eg.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 #include "watcher/watcher.hpp"
 using namespace std;
 
-void load(const string& path) {
-  cout << "load!\n";
+static void load(const string& path, void* content) {
+  ifstream ifs(path);
+  ostringstream ss;
+  ss << ifs.rdbuf();
+  static_cast<string*>(content)->assign(ss.str());
 }
 
+class File {
+ public:
+  File(const string& path) : watcher_(path), content_() {}
+  ~File() {}
+  bool reload_if_renamed() {
+    bool ok = watcher_.if_renamed(load, &content_);
+    cout << "[" << content_ << "]\n";
+    return ok;
+  }
+ private:
+  Watcher watcher_;
+  string content_;
+};
+
 int main() {
-  Watcher watcher("README.md");
+  File file("hoge.txt");
   while (true) {
-    watcher.if_renamed(load);
-    sleep(1);
+    file.reload_if_renamed();
+    sleep(5);
   }
   return 0;
 }
