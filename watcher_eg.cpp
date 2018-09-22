0b16464984a035199ce17960f8c846b898891d74
Shoichi Kaji <skaji@cpan.org>
2018-09-23 00:44:32 +0900

    update

--- watcher_eg.cpp
+++ watcher_eg.cpp
@@ -2,10 +2,14 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <map>
+#include <json/json.h>
 #include "watcher/watcher.hpp"
 using namespace std;
 
-static void load(const string& path, void* content) {
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
+static void load(const string& path, void* out) {
   ifstream ifs(path);
   if (!ifs) {
     cout << "err!\n";
@@ -13,7 +17,22 @@ static void load(const string& path, void* content) {
   }
   ostringstream ss;
   ss << ifs.rdbuf();
-  static_cast<string*>(content)->assign(ss.str());
+  Json::Value root;
+  if (!Json::Reader().parse(ss.str(), root)) {
+    return;
+  }
+  if (!root.isObject()) {
+    return;
+  }
+  map<string, string> m;
+  for (Json::Value::const_iterator it = root.begin(); it != root.end(); ++it) {
+    if (it->isString()) {
+      m[it.key().asString()] = it->asString();
+    } else {
+      return;
+    }
+  }
+  *static_cast<map<string, string>*>(out) = m;
 }
 
 class File {
@@ -23,15 +42,22 @@ class File {
   bool reload_if_renamed() {
     return watcher_.if_renamed(load, &content_);
   }
+  void dump() {
+    cout << "------- dump\n";
+    for (map<string, string>::const_iterator it = content_.begin(); it != content_.end(); ++it) {
+      cout << it->first << ": " << it->second << "\n";
+    }
+  }
  private:
   Watcher watcher_;
-  string content_;
+  map<string, string> content_;
 };
 
 int main() {
-  File file("hoge.txt");
+  File file("hoge.json");
   while (true) {
     file.reload_if_renamed();
+    file.dump();
     sleep(5);
   }
   return 0;
